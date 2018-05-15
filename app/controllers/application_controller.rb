3ef776971c67fd37fea96bfb8eedbad823b06c02
Piotr Wald <valdpiotr@gmail.com>
2018-05-15 22:12:25 +0200

    x

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
+  	return nil
 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
